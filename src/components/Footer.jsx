9fce8b5b673f0e5e29a803ffce75f95b3aafc26a
webhostdevs <webhostdev59@gmail.com>
2024-12-10 13:57:42 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,13 @@ import { Facebook, Instagram, Youtube, Send } from 'lucide-react';
 const Footer = () => {
   return (
     <footer className="min-w-screen bg-gray-800 text-white py-8">
-      <div className=" mx-auto px-4">
-        <div className="grid grid-cols-1 mt-4 md:grid-cols-2 ">
+     <div className="grid grid-cols-1 mt-4 md:grid-cols-2 ">
           {/* Social Media Links */}
           <div className="flex justify-center md:justify-start space-x-6">
             <a href="#" className="hover:text-green-400"><Facebook /></a>
             <a href="#" className="hover:text-green-400"><Instagram /></a>
             <a href="#" className="hover:text-green-400"><Youtube /></a>
           </div>
-
-          {/* Copyright */}
-        
-        </div>
         <div className="text-center mx-auto   text-gray-400">
           Copyright ©2024 All rights reserved | by mahaspice.in
         </div>
