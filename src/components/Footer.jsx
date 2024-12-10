324df10a34d14e7dda4b98a7f6cb852be734998f
webhostdevs <webhostdev59@gmail.com>
2024-12-10 13:56:06 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-// src/components/Footer.jsx
 import React from 'react';
 import { Facebook, Instagram, Youtube, Send } from 'lucide-react';
 
@@ -6,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="min-w-screen bg-gray-800 text-white py-8">
       <div className=" mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 mt-4 md:grid-cols-2 ">
           {/* Social Media Links */}
           <div className="flex justify-center md:justify-start space-x-6">
             <a href="#" className="hover:text-green-400"><Facebook /></a>
@@ -14,25 +13,14 @@ const Footer = () => {
             <a href="#" className="hover:text-green-400"><Youtube /></a>
           </div>
 
-          {/* Newsletter Subscription */}
-          <div className="flex items-center justify-center md:justify-end">
-            <div className="flex">
-              <input
-                type="email"
-                placeholder="Subscribe to newsletter"
-                className="px-4 py-2 rounded-l-md focus:outline-none text-gray-800"
-              />
-              <button className="bg-green-600 px-4 py-2 rounded-r-md hover:bg-green-700">
-                <Send size={20} />
-              </button>
-            </div>
-          </div>
+          {/* Copyright */}
+        
         </div>
-
-        {/* Copyright */}
-        <div className="text-center mt-8 text-gray-400">
+        <div className="text-center mx-auto   text-gray-400">
           Copyright ©2024 All rights reserved | by mahaspice.in
         </div>
+
+        
       </div>
     </footer>
   );
