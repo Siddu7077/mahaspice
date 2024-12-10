27435cc881c9817b1df7d9093c014650ced95488
webhostdevs <webhostdev59@gmail.com>
2024-12-10 13:58:31 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
     <footer className="min-w-screen bg-gray-800 text-white py-8">
      <div className="grid grid-cols-1 mt-4 md:grid-cols-2 ">
           {/* Social Media Links */}
-          <div className="flex justify-center md:justify-start space-x-6">
+{/*           <div className="flex justify-center md:justify-start space-x-6">
             <a href="#" className="hover:text-green-400"><Facebook /></a>
             <a href="#" className="hover:text-green-400"><Instagram /></a>
             <a href="#" className="hover:text-green-400"><Youtube /></a>
-          </div>
+          </div> */}
         <div className="text-center mx-auto   text-gray-400">
           Copyright ©2024 All rights reserved | by mahaspice.in
         </div>
