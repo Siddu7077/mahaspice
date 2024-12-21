780957b370149b346804faf175fbac0045c06693
“Mudabbir” <sohailmudabbir@gmail.com>
2024-12-22 02:21:32 +0530

    SHA1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -194,32 +194,6 @@ const Navbar = () => {
 
   return (
     <div className="flex h-screen bg-white text-black ">
-    <Helmet>
-        {/* Basic Meta Tags */}
-        <title>Mahaspice | Powered by Webhost Devs</title>
-        <meta 
-          name="description" 
-          content="Discover Mahaspice, the ultimate catering service provider powered by Webhost Devs. Premium event catering solutions with a tech-savvy touch." 
-        />
-        <meta name="keywords" content="Mahaspice, Webhost Devs, Catering Services, Website Development, Tech Solutions" />
-        <meta name="author" content="Webhost Devs" />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://www.mahaspice.in/" />
-        
-        {/* Open Graph Tags for Social Media */}
-        <meta property="og:title" content="Mahaspice | Powered by Webhost Devs" />
-        <meta property="og:description" content="Mahaspice offers exceptional catering services with the technical expertise of Webhost Devs, ensuring seamless operations and customer satisfaction." />
-        <meta property="og:url" content="https://www.mahaspice.in/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Mahaspice | Webhost Devs" />
-        <meta property="og:image" content="https://www.mahaspice.in/assets/branding-image.jpg" />
-
-        {/* Twitter Card Meta */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Mahaspice | Powered by Webhost Devs" />
-        <meta name="twitter:description" content="Discover Mahaspice and Webhost Devs' collaboration for exceptional catering and technology solutions." />
-        <meta name="twitter:image" content="https://www.mahaspice.in/assets/branding-image.jpg" />
-      </Helmet>
       <ScrollToTop />
       {/* Left Sidebar */}
       <div
